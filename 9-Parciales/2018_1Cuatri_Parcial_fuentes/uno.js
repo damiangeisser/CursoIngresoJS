9b4ec4389fe66f26546fcb75dbd16af4ac056eed
damiangeisser <dageisser@gmail.com>
2019-02-13 09:12:07 -0300

    Parcial 13 Feb

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -2,4 +2,39 @@
 function mostrar()
 {
 
+	var nombre1;
+	var nombre2;
+	var edad1;
+	var edad2;
+	var sumaEdades;
+
+	nombre1 = prompt("Por favor ingrese el primer nombre y presione aceptar:");
+
+	edad1 = prompt("Por favor ahora ingrese la edad de " + nombre1 +" y presione aceptar:" );
+	edad1 = parseInt(edad1);
+
+	nombre2 = prompt("Por favor ingrese el segundo nombre y presione aceptar:");
+
+	edad2 = prompt("Por favor ahora ingrese la edad de " + nombre2 +" y presione aceptar:" );
+	edad2 = parseInt(edad2);
+
+	sumaEdades = edad1 + edad2;
+
+	alert("Ustedes son " + nombre1 + " y " + nombre2 + ", sus edades son " + edad1 + " y " + edad2 + ", y la suma de sus edades es " + sumaEdades);
+
+
+// var altura;
+// var anchura;
+// var perimetro;
+
+// altura = prompt("Por favor ingrese el alto del rectángulo y presione aceptar.");
+
+// altura = parseInt(altura);
+
+// anchura = prompt("Por favor ingrese el ancho del rectángulo y presione aceptar.");
+
+// anchura = parseInt(anchura);
+
+// perimetro = (anchura + altura) * 2;
+
 }
