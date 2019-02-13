60f043e8228fe60fe0c3f1acea3fa7224d7846f9
damiangeisser <dageisser@gmail.com>
2019-02-13 10:56:27 -0300

    Parcial 13 Feb

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -8,6 +8,7 @@ function mostrar()
 	var recargoTarjeta;
 	var precioFinal;
 	var pagaConTarjeta;
+	var mensaje;
 
 	descuentoCantidad = 0.9;
 
@@ -18,43 +19,51 @@ function mostrar()
 	cantidadProductos = prompt("Por favor ingrese la cantidad de productos comprados y presione aceptar:");
 	cantidadProductos = parseInt(cantidadProductos);
 
+	mensaje = "Usted compró " + cantidadProductos + " productos";
+
 	montoCompra = prompt("Por favor ingrese el monto de la compra y presione aceptar:");
 	montoCompra = parseInt(montoCompra);
 
 	precioFinal = montoCompra;
 
-	pagaConTarjeta = prompt("Por favor, si el pago es con tarjeta ingrese si:");
+	//pagaConTarjeta = prompt("Por favor, si el pago es con tarjeta ingrese si:");
+
+	pagaConTarjeta = prompt("Por favor, si el pago es con tarjeta ingrese 1 de lo contario, ingrese 0:");
+	pagaConTarjeta = parseInt(pagaConTarjeta);
 
 	if(cantidadProductos > 2)
 	{
 	
 		precioFinal = montoCompra * descuentoCantidad;
+		precioFinal = precioFinal.toFixed(2);
+
+		mensaje = mensaje + " (10% de descuento por cantidad)"
 		
 		if(montoCompra > 2000)
 		{
 
 		precioFinal = precioFinal * descuentoMonto;
+		precioFinal = precioFinal.toFixed(2);
+
+		mensaje = mensaje + ", superando los $2000 (15% de descuento adicional)"
 
 		}
 	
 	}
 
 
-	if(pagaConTarjeta == 'si')
+	//if(pagaConTarjeta == 'si')
+	if(pagaConTarjeta)
 	{
 
 		precioFinal = precioFinal * recargoTarjeta;
+		precioFinal = precioFinal.toFixed(2);
 
-	 	alert("Se compraron " + cantidadProductos + " productos por un total de $" + montoCompra + " a pagar con tarjeta siendo el monto final $" + precioFinal + " incluyendo los descuentos aplicables y un recargo de 10% por el método de pago.");
-
-	}
-	else
-	{
-
-		alert("Se compraron " + cantidadProductos + " productos por un total de " + montoCompra + " a pagar en efectivo siendo el total" + precioFinal + "incluyendo los descuentos aplicables.");
+		mensaje = mensaje + " y paga con tarjeta (10% de recargo)";
 
 	}
-
+	
+	alert(mensaje + ". Monto final: $" + precioFinal);
 
 
 }
