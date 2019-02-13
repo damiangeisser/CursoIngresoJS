0d457e1762a760bedfcbc6aa2de81cbcc43912a4
damiangeisser <dageisser@gmail.com>
2019-02-13 09:21:51 -0300

    Parcial 13 Feb

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,20 @@
 function mostrar()
 {
   
+	var precioLista;
+	var recargoTarjeta;
+	var precioTarjeta;
+
+	precioLista = prompt("Por favor ingrese el precio de lista y presione aceptar:");
+	precioLista = parseInt(precioLista);
+
+	recargoTarjeta = precioLista * 0.1;
+
+	precioTarjeta = precioLista + recargoTarjeta;
+
+	alert("El precio del producto en efectivo es: $" + precioLista + ", con tarjeta tiene un recargo del 10% que sería $" + recargoTarjeta + " de recargo, costándole final $" + precioTarjeta);
+
+
+
+
 }
