8add66d2d0e7b785bd394ea9d90714d4a5b2be13
damiangeisser <dageisser@gmail.com>
2019-02-19 07:27:55 -0300

    Tarea Feb 19

--- 8-TPs/jsPiedarPapelTijera1.js
+++ 8-TPs/jsPiedarPapelTijera1.js
@@ -5,25 +5,83 @@ El jugador seleccionará una imagen correspondiente
 a su opción  y le informaremos si ganó, empató o perdió.
 */
 var eleccionMaquina;
+var elecccionJugador;
+var mensaje;
 
 function comenzar()
 {
 	
+eleccionMaquina = Math.floor((Math.random() * 3) + 1);
 
+//Valores de eleccionMaquina: 1 piedra, 2 papel, 3 tijera.
 
 }//FIN DE LA FUNCIÓN
 function piedra()
 {
 	
+	if(eleccionMaquina == 1)
+	{
+		mensaje = "El ordenador eligió piedra. Empate.";
+	}
+	else
+	{
+		if(eleccionMaquina == 2)
+		{
+			mensaje = "El ordenador eligió papel. Perdió.";
+		}
+		else
+		{
+			mensaje = "El ordenador eligió tijera. Ganó.";
+		}
+
+	}
+
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
 function papel()
 {
 
+	if(eleccionMaquina == 1)
+	{
+		mensaje = "El ordenador eligió piedra. Ganó.";
+	}
+	else
+	{
+		if(eleccionMaquina == 2)
+		{
+			mensaje = "El ordenador eligió papel. Empate.";
+		}
+		else
+		{
+			mensaje = "El ordenador eligió tijera. Perdió.";
+		}
+
+	}
+
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
 	
+	if(eleccionMaquina == 1)
+	{
+		mensaje = "El ordenador eligió piedra. Perdió.";
+	}
+	else
+	{
+		if(eleccionMaquina == 2)
+		{
+			mensaje = "El ordenador eligió papel. Ganó.";
+		}
+		else
+		{
+			mensaje = "El ordenador eligió tijera. Empate.";
+		}
+
+	}
+
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
