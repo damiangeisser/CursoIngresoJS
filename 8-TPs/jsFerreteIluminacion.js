00b608b65631d2936a362f7b06476d140e7e1888
damiangeisser <dageisser@gmail.com>
2019-02-19 10:50:26 -0300

    Clase Feb 19

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -8,7 +8,7 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
  */
-function CalcularPrecio () 
+function CalcularPrecio() 
 {
 
 	var cantidadLamparas;
@@ -17,58 +17,66 @@ function CalcularPrecio ()
  	var ingresosBrutos;
  	var totalFinal;
 
- 	cantidadLamparas = Cantidad.value;
+ 	cantidadLamparas = document.getElementById('Cantidad').value;
  	cantidadLamparas = parseInt(cantidadLamparas);
- 	marcaLamparas = Marca.value;
+ 	marcaLamparas = document.getElementById('Marca').value;
 
  	total = 35 * cantidadLamparas;
 
- 	if(cantidadLamparas >= 3 && <=5 )
- 	{
- 		switch(cantidadLamparas)
- 		{
- 		case "3":
- 			if(marcaLamparas == "ArgentinaLuz")
- 			{
- 				total = total * 0.85;
- 			}
- 			if(marcaLamparas == "FelipeLamparas")
- 			{
- 				total = total * 0.9;
- 			}
- 			else
- 			{
- 				total = total * 0.95;
- 			}
- 		break;
- 		case "4":
- 			if(marcaLamparas == "ArgentinaLuz" || marcaLamparas == "FelipeLamparas")
- 			{
- 				total = total * 0.75;
- 			}
- 			else
- 			{
- 				total = total * 0.8;
- 			}
- 		break;
- 		case "5":
- 			if(marcaLamparas == "ArgentinaLuz")
- 			{
- 				total = total * 0.6;
- 			}
- 			else
- 			{
- 				total = total * 0.7;
- 			}
- 		break;
- 		default:
- 		}
- 	}
+ 	console.log(cantidadLamparas);
 
- 	if(cantidadLamparas >= 6)
+if(cantidadLamparas >= 6)
  	{
  		total = total * 0.5;
  	}
+ 	else
+ 	{
+	 	if(cantidadLamparas >= 3 && cantidadLamparas <= 5 )
+	 	{
+	 		switch(cantidadLamparas)
+	 		{
+	 		case 3:
+	 			if(marcaLamparas == "ArgentinaLuz")
+	 			{
+	 				total = total * 0.85;
+	 			}
+	 			else
+	 			{
+		 			if(marcaLamparas == "FelipeLamparas")
+		 			{
+		 				total = total * 0.9;
+		 			}
+		 			else
+		 			{
+		 				total = total * 0.95;
+		 			}
+		 		}	
+	 			break;
+	 		case 4:
+	 			if(marcaLamparas == "ArgentinaLuz" || marcaLamparas == "FelipeLamparas")
+	 			{
+	 				total = total * 0.75;
+	 			}
+	 			else
+	 			{
+	 				total = total * 0.8;
+	 			}
+	 			break;
+	 		case 5:
+	 			if(marcaLamparas == "ArgentinaLuz")
+	 			{
+	 				total = total * 0.6;
+	 			}
+	 			else
+	 			{
+	 				total = total * 0.7;
+	 			}
+	 			break;
+	 		default:
+	 			break;
+	 		}
+	 	}// if 3 < cantidad < 6
+	 }// if cantidad > 6
 
  	if(total > 120)
  	{
@@ -79,8 +87,88 @@ function CalcularPrecio ()
  	}
  	else
  	{
+ 		ingresosBrutos = 0;
  		totalFinal = total;
- 	}
 
+ 		precioDescuento.value = "El precio final es: $" + totalFinal + ".- IIBB usted pagó: $" + ingresosBrutos + ".-";
+ 	}// if monto supera los $120
 
+console.log(total);
+	// var cantidadLamparas;
+	// var descuento;
+ // 	var marca;
+	// var precioBruto;
+ // 	var precioDescuento;
+
+ // 	cantidadLamparas = document.getElementById('Cantidad').value;
+
+ // 	cantidadLamparas = parseInt(cantidadLamparas);
+
+ // 	marca = document.getElementById('Marca').value;
+
+ // 	precioBruto = cantidadLamparas * 35;
+
+ // 	if(cantidadLamparas > 5)
+ // 	{
+ // 		descuento = 50;
+ // 	}
+ // 	else
+ // 	{
+ // 		if(cantidadLamparas == 5)
+ // 		{
+ // 			if(marca == "ArgentinaLuz")
+ // 			{
+ // 				descuento = 40;	
+ // 			}
+ // 			else
+ // 			{
+ // 				descuento = 30;	
+ // 			}// if marca ArgentinaLuz
+ // 		}
+ // 		else
+ // 		{
+
+ // 			if(cantidadLamparas == 4)
+ // 			{
+ // 				if(marca == "ArgentinaLuz" || marca == "FelipeLamparas" )
+ // 				{
+ // 					descuento = 25;	
+
+ // 				}
+ // 				else
+ // 				{
+ // 					descuento = 20;
+ // 				}// if marca ArgentinaLuz o FelipeLamparas
+ // 			}
+ // 			else
+ // 			{
+ // 				if(cantidadLamparas == 3)
+ // 				{
+ // 					if(marca == "ArgentinaLuz")
+ // 					{
+ // 						descuento = 15;
+ // 					}
+ // 					else
+ // 					{
+ // 						if(marca == "FelipeLamparas")
+ // 						{
+ // 							descuento = 10;
+ // 						}
+ // 						else
+ // 						{
+ // 							descuento = 5;
+ // 						}// if marca FelipeLamparas
+ // 					}// if marca ArgentinaLuz
+ // 				}
+ // 				else
+ // 				{
+ // 					descuento = 100;
+ // 				}// if 3 lámparas
+ // 			}// if 4 lámparas
+ // 		}// if 5 lámparas
+ // 	}// if más de 5 lámparas
+
+ // 	precioDescuento = precioBruto * descuento / 100;
+
+ // 	console.log("precio" + precioDescuento);
 }
--- 8-TPs/jsPiedarPapelTijera2.js
+++ 8-TPs/jsPiedarPapelTijera2.js
@@ -7,25 +7,105 @@ function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 3
 	 	
+	eleccionMaquina = Math.floor((Math.random() * 3) + 1);
 
+	//Valores de eleccionMaquina: 1 piedra, 2 papel, 3 tijera.
 
 
 }//FIN DE LA FUNCIÓN
 function piedra()
 {
 	
+	if(eleccionMaquina == 1)
+	{
+		mensaje = "El ordenador eligió piedra. Empate.";
+
+		ContadorDeEmpates++;
+	}
+	else
+	{
+		if(eleccionMaquina == 2)
+		{
+			mensaje = "El ordenador eligió papel. Perdió.";
+
+			ContadorDePerdidas++;
+		}
+		else
+		{
+			mensaje = "El ordenador eligió tijera. Ganó.";
+
+			ContadorDeGanadas++;
+		}
+
+	}
+
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
 function papel()
 {
 
+	if(eleccionMaquina == 1)
+	{
+		mensaje = "El ordenador eligió piedra. Ganó.";
+
+		ContadorDeGanadas++;
+	}
+	else
+	{
+		if(eleccionMaquina == 2)
+		{
+			mensaje = "El ordenador eligió papel. Empate.";
+
+			ContadorDeEmpates++;
+		}
+		else
+		{
+			mensaje = "El ordenador eligió tijera. Perdió.";
+
+			ContadorDePerdidas++;
+		}
+
+	}
+
+	alert(mensaje);
+
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
 	
+	if(eleccionMaquina == 1)
+	{
+		mensaje = "El ordenador eligió piedra. Perdió.";
+
+		ContadorDePerdidas++;
+	}
+	else
+	{
+		if(eleccionMaquina == 2)
+		{
+			mensaje = "El ordenador eligió papel. Ganó.";
+
+			ContadorDeGanadas++;
+		}
+		else
+		{
+			mensaje = "El ordenador eligió tijera. Empate.";
+
+			ContadorDeEmpates++;
+		}
+
+	}
+
+	alert(mensaje);
+
 }//FIN DE LA FUNCIÓN
 
 function mostrarResultado()
 {
+
+	ganadas.value=ContadorDeGanadas;
+	perdidas.value=ContadorDePerdidas;
+	empatadas.value=ContadorDeEmpates;
 
 }
