773eb6cc3ddb6948e94eb5d4f85c53f14725c7f7
damiangeisser <dageisser@gmail.com>
2019-02-20 10:13:20 -0300

    Parcial Feb 20

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,69 +1,112 @@
 function mostrar()
 {
 
-	var cantidadProductos;
-	var montoCompra;
-	var descuentoCantidad;
-	var descuentoMonto;
-	var recargoTarjeta;
-	var precioFinal;
-	var pagaConTarjeta;
+	var numeroUno;
+	var numeroDos;
+	var suma;
+	var resta;
 	var mensaje;
 
-	descuentoCantidad = 0.9;
+	numeroUno = prompt("Por favor ingrese el valor del 1er número y presione aceptar:");
+	numeroUno = parseInt(numeroUno);
 
-	descuentoMonto = 0.85;
+	numeroDos = prompt("Por favor ingrese el valor del 2do número y presione aceptar:");
+	numeroDos = parseInt(numeroDos);
 
-	recargoTarjeta = 1.1;
-
-	cantidadProductos = prompt("Por favor ingrese la cantidad de productos comprados y presione aceptar:");
-	cantidadProductos = parseInt(cantidadProductos);
-
-	mensaje = "Usted compró " + cantidadProductos + " productos";
-
-	montoCompra = prompt("Por favor ingrese el monto de la compra y presione aceptar:");
-	montoCompra = parseInt(montoCompra);
-
-	precioFinal = montoCompra;
-
-	//pagaConTarjeta = prompt("Por favor, si el pago es con tarjeta ingrese si:");
-
-	pagaConTarjeta = prompt("Por favor, si el pago es con tarjeta ingrese 1 de lo contario, ingrese 0:");
-	pagaConTarjeta = parseInt(pagaConTarjeta);
-
-	if(cantidadProductos > 2)
+	if(numeroUno == numeroDos)
 	{
-	
-		precioFinal = montoCompra * descuentoCantidad;
-		precioFinal = precioFinal.toFixed(2);
-
-		mensaje = mensaje + " (10% de descuento por cantidad)";
-		
-		if(montoCompra > 2000)
+		mensaje = numeroUno + "" + numeroDos;
+	}
+	else
+	{
+		if(numeroUno > numeroDos)
 		{
+			resta = numeroUno - numeroDos;
 
-		precioFinal = precioFinal * descuentoMonto;
-		precioFinal = precioFinal.toFixed(2);
-
-		mensaje = mensaje + ", superando los $2000 (15% de descuento adicional)";
+			mensaje = "La resta da: " + resta;
 
 		}
+		else
+		{
+			suma = numeroUno + numeroDos;
+			if(suma > 10)
+			{
+				mensaje = "La suma es " + suma + " y superó el 10.";
+			}
+			else
+			{
+				mensaje = "La suma da: " + suma;
+			}// if suma > 10
+		}// if numeroUno > numeroDos
+	}// if numeroUno == numeroDos
+
+	alert(mensaje);
+
+	//Código del parcial anterior:
+
+	// var cantidadProductos;
+	// var montoCompra;
+	// var descuentoCantidad;
+	// var descuentoMonto;
+	// var recargoTarjeta;
+	// var precioFinal;
+	// var pagaConTarjeta;
+	// var mensaje;
+
+	// descuentoCantidad = 0.9;
+
+	// descuentoMonto = 0.85;
+
+	// recargoTarjeta = 1.1;
+
+	// cantidadProductos = prompt("Por favor ingrese la cantidad de productos comprados y presione aceptar:");
+	// cantidadProductos = parseInt(cantidadProductos);
+
+	// mensaje = "Usted compró " + cantidadProductos + " productos";
+
+	// montoCompra = prompt("Por favor ingrese el monto de la compra y presione aceptar:");
+	// montoCompra = parseInt(montoCompra);
+
+	// precioFinal = montoCompra;
+
+	// //pagaConTarjeta = prompt("Por favor, si el pago es con tarjeta ingrese si:");
+
+	// pagaConTarjeta = prompt("Por favor, si el pago es con tarjeta ingrese 1 de lo contario, ingrese 0:");
+	// pagaConTarjeta = parseInt(pagaConTarjeta);
+
+	// if(cantidadProductos > 2)
+	// {
 	
-	}
+	// 	precioFinal = montoCompra * descuentoCantidad;
+	// 	precioFinal = precioFinal.toFixed(2);
 
+	// 	mensaje = mensaje + " (10% de descuento por cantidad)";
+		
+	// 	if(montoCompra > 2000)
+	// 	{
 
-	//if(pagaConTarjeta == 'si')
-	if(pagaConTarjeta)
-	{
+	// 	precioFinal = precioFinal * descuentoMonto;
+	// 	precioFinal = precioFinal.toFixed(2);
 
-		precioFinal = precioFinal * recargoTarjeta;
-		precioFinal = precioFinal.toFixed(2);
+	// 	mensaje = mensaje + ", superando los $2000 (15% de descuento adicional)";
 
-		mensaje = mensaje + " y paga con tarjeta (10% de recargo)";
-
-	}
+	// 	}
 	
-	alert(mensaje + ". Monto final: $" + precioFinal);
+	// }
+
+
+	// //if(pagaConTarjeta == 'si')
+	// if(pagaConTarjeta)
+	// {
+
+	// 	precioFinal = precioFinal * recargoTarjeta;
+	// 	precioFinal = precioFinal.toFixed(2);
+
+	// 	mensaje = mensaje + " y paga con tarjeta (10% de recargo)";
+
+	// }
+	
+	// alert(mensaje + ". Monto final: $" + precioFinal);
 
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,82 @@
+//ingresar 4 importes del pizarron moostrar el mayor, mostrar total,
+//si supera 100 aplicar 10% descuento al total, si supera 50 descuento 5%,
+//si menor a 50 sumar 15% al total 
+
 function mostrar()
 {
+
+	var numeroUno;
+	var numeroDos;
+	var numeroTres;
+	var numeroCuatro;
+	var modificadorTotal;
+	var mensajeMayor;
+	var total;
+	var totalFinal;
+	var mensaje;
+
+
+	numeroUno = prompt("Por favor ingrese el primer monto y presione aceptar.");
+	numeroUno = parseInt(numeroUno);
+
+	numeroDos = prompt("Por favor ingrese el segundo monto y presione aceptar.");
+	numeroDos = parseInt(numeroDos);
+
+	numeroTres = prompt("Por favor ingrese el tercer monto y presione aceptar.");
+	numeroTres = parseInt(numeroTres);
+
+	numeroCuatro = prompt("Por favor ingrese el cuarto monto y presione aceptar.");
+	numeroCuatro = parseInt(numeroCuatro);
+	
+
+	if(numeroUno > numeroDos && numeroUno > numeroTres && numeroUno > numeroCuatro)
+	{
+		mensajeMayor = " Y $" + numeroUno + " es el mayor de los montos.";
+	}
+	else
+	{
+		if(numeroDos > numeroTres && numeroDos > numeroCuatro)
+		{
+			mensajeMayor = " Y $" + numeroDos + " es el mayor de los montos.";
+		}
+		else
+		{
+			if(numeroTres > numeroCuatro)
+			{
+			
+				mensajeMayor = " Y $" + numeroTres + " es el mayor de los montos.";
+
+			}
+			else
+			{
+				mensajeMayor = " Y $" + numeroCuatro + " es el mayor de los montos.";
+
+			}// if numeroTres > numeroCuatro
+		}// if numeroDos > numeroTres && numeroDos > numeroCuatro
+	}// if numeroUno > numeroDos && numeroUno > numeroTres && numeroUno > numeroCuatro
+
+	total = numeroUno + numeroDos + numeroTres + numeroCuatro;
+
+	if(total > 100)
+	{
+		modificadorTotal = 0.9;
+	}
+	else
+	{
+		if(total > 50)
+		{
+			modificadorTotal = 0.95;
+		}
+		else
+		{
+			modificadorTotal = 1.15;
+		}
+	}
+
+	totalFinal = total * modificadorTotal;
+
+	mensaje = "El total de la compra es: $" + totalFinal;
+
+	alert(mensaje + mensajeMayor);
 
 }
